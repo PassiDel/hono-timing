21cc287a8245c5141f1d621a77c505cdb8222400
Pascal Syma <pascal@syma.dev>
2023-06-30 16:08:10 +0200

    Add content to README

--- example/index.ts
+++ example/index.ts
@@ -1,9 +1,31 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
-import { timing } from '@puazzi/hono-timing';
+import { endTime, setMetric, startTime, timing } from '@puazzi/hono-timing';
 
 const app = new Hono();
-app.use('*', timing());
-app.get('/', (c) => c.text('Hello Hono!'));
+
+app.use(
+  '*',
+  timing({
+    enabled: (c) => c.req.method === 'GET'
+  })
+);
+
+app.get('/', async (c) => {
+  // add custom metrics
+  setMetric(c, 'region', 'europe-west3');
+
+  // add custom metrics with timing, must be in milliseconds
+  setMetric(c, 'custom', 23.8, 'My custom Metric');
+
+  // start a new timer
+  startTime(c, 'db');
+
+  await new Promise((r) => setTimeout(r, 187));
+  // end the timer
+  endTime(c, 'db');
+
+  return c.json({ response: '' });
+});
 
 serve(app);
